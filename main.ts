869f40b5005a331939f735ac70fa25c0c5da58d0
nahernande <120121658+nahernande@users.noreply.github.com>
2022-12-13 09:00:21 -0700

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -27,32 +27,7 @@ sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, ot
 info.onLifeZero(function () {
     game.over(false)
 })
-let Ghost: Sprite = null
-let projectile: Sprite = null
-let mySprite: Sprite = null
-tiles.setCurrentTilemap(tilemap`level1`)
-mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite)
-mySprite.setPosition(41, 33)
-scene.cameraFollowSprite(mySprite)
-game.onUpdateInterval(5000, function () {
+function ghosts () {
     Ghost = sprites.createProjectileFromSide(img`
         ........................
         ........................
@@ -79,7 +54,38 @@ game.onUpdateInterval(5000, function () {
         ........................
         ........................
         `, 0, 50)
+    Ghost.setPosition(27, 110)
     Ghost.x = randint(0, 0)
     Ghost.setKind(SpriteKind.Enemy)
     Ghost.follow(mySprite, 30)
+}
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
 })
+let Ghost: Sprite = null
+let projectile: Sprite = null
+let mySprite: Sprite = null
+tiles.setCurrentTilemap(tilemap`level1`)
+mySprite = sprites.create(img`
+    . . . . . . f f f f . . . . . . 
+    . . . . f f f 2 2 f f f . . . . 
+    . . . f f f 2 2 2 2 f f f . . . 
+    . . f f f e e e e e e f f f . . 
+    . . f f e 2 2 2 2 2 2 e e f . . 
+    . . f e 2 f f f f f f 2 e f . . 
+    . . f f f f e e e e f f f f . . 
+    . f f e f b f 4 4 f b f e f f . 
+    . f e e 4 1 f d d f 1 4 e e f . 
+    . . f e e d d d d d d e e f . . 
+    . . . f e e 4 4 4 4 e e f . . . 
+    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . . f f . . f f . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite)
+mySprite.setPosition(41, 33)
+info.setLife(3)
+scene.cameraFollowSprite(mySprite)
+ghosts()
